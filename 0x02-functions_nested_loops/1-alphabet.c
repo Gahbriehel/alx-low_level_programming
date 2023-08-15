1557639e7b6034b006b975845e32b6fa70094c1d
Gahbriehel <babatise002@gmail.com>
2023-08-15 11:29:43 -0700

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,9 +4,9 @@
  */
 void print_alphabet(void)
 {
-	char alphabet;
+	char letter;
 
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-			_putchar(alphabet);
+	for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
 	_putchar('\n');
 }
