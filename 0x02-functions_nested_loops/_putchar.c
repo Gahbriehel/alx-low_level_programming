43d5caa02ffe161c1196fd18608f08d9447cc3ea
Gahbriehel <babatise002@gmail.com>
2023-08-15 10:30:01 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*_putchar: writes char c to stdout
+*@c: The char to print
+*
+* Return: 1 (success)
+* Return: 0 (error)
+*/
+int _putchar(char c)
+{
+	return (write(1, &c 1));
+}
