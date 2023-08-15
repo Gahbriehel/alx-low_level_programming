ded9d2b07bf16da9aa400a28e59656a13e3b1560
Gahbriehel <babatise002@gmail.com>
2023-08-15 10:39:46 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char alphabet
+		for (alphabet = 'a'; letter <= 'z'; alphabet++)
+			_putchar(alphabet);
+	_putchar('\n');
+}
