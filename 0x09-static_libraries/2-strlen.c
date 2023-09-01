2e37d6f4397f520635ca8876cc5de8eadb9b363a
Gahbriehel <babatise002@gmail.com>
2023-09-01 11:18:10 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -2,19 +2,18 @@
 /**
  * _strlen - returns the length of a string
  * @s: string
+ *
  * Return: length
  */
 int _strlen(char *s)
 {
-	int longi = 0;
-
+	int lent = 0;
 
 	while (*s != '\0')
 	{
-		longi++;
+		lent++;
 		s++;
 	}
 
-
-	return (longi);
+	return (lent);
 }
